99baef4c76bd330261336481226f986e56af000f
James Pier <jpier2012@gmail.com>
2019-04-16 11:58:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,7 @@
 class Dog
   def bark
-    puts "Wooooooof!"
+    puts "Woof!"
   end
 end
 
-def fido.bark_bark
-  puts "Woof wooooooof!"
-end
-
-#Dog.bark
-fido.bark
-fido.bark_bark
-
-def Dog.self.bark_bark_bark
-  puts "Woof, woof, woof"
-end
-
-lassie = Dog.new
-
-lassie.bark
-lassie.bark_bark
-
-lassie.bark_bark_bark
+Dog.bark
