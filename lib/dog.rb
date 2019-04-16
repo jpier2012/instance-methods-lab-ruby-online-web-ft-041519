3042a763a16de1476e5692683f7fbe7f9754f921
James Pier <jpier2012@gmail.com>
2019-04-16 12:14:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,13 @@
 class Dog
-
-  @@class_time = Time.now
-
-  def initialize
-    @object_time = Time.now
+  def sit
+    puts "The Dog is sitting"
   end
 
-  def self.bark
+
+  def bark
     puts "Woof!"
-  end
-
-  def self.class_get_time
-    puts "#{@@class_time} This is class time"
-  end
-
-  def object_get_time
-    puts "#{@object_time} This is object time"
   end
 end
 
 fido = Dog.new
 
-Dog.class_get_time
-#Dog.object_get_time ## doesn't work
-
-#fido.class_get_time ## doesn't work
-fido.object_get_time
